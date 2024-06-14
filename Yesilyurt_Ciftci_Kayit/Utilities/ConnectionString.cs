463e8c09bcad77a2c107b8fac74995b5f3c55494
çağlar yurdakul <37529684+caglary@users.noreply.github.com>
2024-06-14 13:54:31 +0300

    14062024

--- Yesilyurt_Ciftci_Kayit/Utilities/ConnectionString.cs
+++ Yesilyurt_Ciftci_Kayit/Utilities/ConnectionString.cs
@@ -9,22 +9,22 @@ namespace Yesilyurt_Ciftci_Kayit.Utilities
         {
             string connectionString = "";
             string machineName = Environment.MachineName;
-            string Lenovo2022 = @"Server=M601102-0009;Database=YesilyurtDb2022;User Id=sa;Password=Password.123;";
-            string Lenovo2021 = @"Server=M601102-0009;Database=YesilyurtDb2021;User Id=sa;Password=Password.123;";
-            string Lenovo2023 = @"Server=M601102-0009;Database=YesilyurtDb2023;User Id=sa;Password=Password.123;";
-            string Lenovo2024 = @"Server=M601102-0009;Database=YesilyurtDb2024;User Id=sa;Password=Password.123;";
+            string Lenovo2022 = @"Server=M601102-0012;Database=YesilyurtDb2022;User Id=sa;Password=Password.123;";
+            string Lenovo2021 = @"Server=M601102-0012;Database=YesilyurtDb2021;User Id=sa;Password=Password.123;";
+            string Lenovo2023 = @"Server=M601102-0012;Database=YesilyurtDb2023;User Id=sa;Password=Password.123;";
+            string Lenovo2024 = @"Server=M601102-0012;Database=YesilyurtDb2024;User Id=sa;Password=Password.123;";
 
 
-            string Work2022 = @"Server=M601102-0009;Database=YesilyurtDb2022;User Id=sa;Password=Password.123;";
-            string Work2021 = @"Server=M601102-0009;Database=YesilyurtDb2021;User Id=sa;Password=Password.123;";
-            string Work2023 = @"Server=M601102-0009;Database=YesilyurtDb2023;User Id=sa;Password=Password.123;";
-            string Work2024 = @"Server=M601102-0009;Database=YesilyurtDb2024;User Id=sa;Password=Password.123;";
+            string Work2022 = @"Server=M601102-0012;Database=YesilyurtDb2022;User Id=sa;Password=Password.123;";
+            string Work2021 = @"Server=M601102-0012;Database=YesilyurtDb2021;User Id=sa;Password=Password.123;";
+            string Work2023 = @"Server=M601102-0012;Database=YesilyurtDb2023;User Id=sa;Password=Password.123;";
+            string Work2024 = @"Server=M601102-0012;Database=YesilyurtDb2024;User Id=sa;Password=Password.123;";
 
 
             string Year = year;
 
 
-            if (machineName == "M601102-0009")
+            if (machineName == "M601102-00099")
             {
                 switch (Year)
                 {
